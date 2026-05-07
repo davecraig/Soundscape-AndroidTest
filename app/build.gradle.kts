a0460c0d9d5bdddb0b579cf4dd3a4b6b0c007e8e
davecraig <8530624+davecraig@users.noreply.github.com>
2026-05-07 13:05:47 +0000

    Bump version to 2.0.6, version code 1006

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ android {
         applicationId = "org.scottishtecharmy.soundscape"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1005
-        versionName = "2.0.5"
+        versionCode = 1006
+        versionName = "2.0.6"
 
         // Maintaining this list means that we can exclude translations that aren't complete yet
         resourceConfigurations.addAll(listOf(
