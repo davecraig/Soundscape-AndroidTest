Scottish-Tech-Army/Soundscape-Android

Switch to using non-Beta Soundscape image on Welcome screen
Getting ready for production release.

app/src/main/java/org/scottishtecharmy/soundscape/screens/onboarding/welcome/WelcomeScreen.kt
@@ -104,7 +104,7 @@ fun Welcome(
 @Composable
 private fun LocalImage() {
     Image(
-        painter = painterResource(R.drawable.soundscape_alpha_1024),
+        painter = painterResource(R.drawable.soundscape_1024),
         contentDescription = null,
         modifier = Modifier
             .clip(RoundedCornerShape(spacing.extraLarge))

davecraig/Soundscape-AndroidTest

shared/src/commonMain/kotlin/org/scottishtecharmy/soundscape/screens/onboarding/welcome/WelcomeScreen.kt
@@ -28,7 +28,7 @@ import org.scottishtecharmy.soundscape.resources.Res
 import org.scottishtecharmy.soundscape.resources.first_launch_welcome_button
 import org.scottishtecharmy.soundscape.resources.first_launch_welcome_description
 import org.scottishtecharmy.soundscape.resources.first_launch_welcome_title
-import org.scottishtecharmy.soundscape.resources.soundscape_alpha_1024
+import org.scottishtecharmy.soundscape.resources.soundscape_1024
 import org.scottishtecharmy.soundscape.screens.onboarding.component.BoxWithGradientBackground
 import org.scottishtecharmy.soundscape.ui.theme.spacing
 
@@ -109,7 +109,7 @@ fun Welcome(
 @Composable
 private fun LocalImage() {
     Image(
-        painter = painterResource(Res.drawable.soundscape_alpha_1024),
+        painter = painterResource(Res.drawable.soundscape_1024),
         contentDescription = null,
         modifier = Modifier
             .clip(RoundedCornerShape(spacing.extraLarge))