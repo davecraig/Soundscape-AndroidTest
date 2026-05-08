5a7091c4e4e01b8cea207f58750ef864085fbb8a
Dave Craig <davecraig@unbalancedaudio.com>
2026-05-08 11:02:26 +0100

    Propagate full version number through into iOS app
    We want 2.0.x to appear in the 2.0 bucket in TestFlight, but it needs to show
    up as 2.0.x in the app drawer menu.

--- shared/src/iosMain/kotlin/org/scottishtecharmy/soundscape/platform/PlatformInfo.kt
+++ shared/src/iosMain/kotlin/org/scottishtecharmy/soundscape/platform/PlatformInfo.kt
@@ -2,8 +2,12 @@ package org.scottishtecharmy.soundscape.platform
 
 import platform.Foundation.NSBundle
 
-actual fun appVersionName(): String =
-    NSBundle.mainBundle.objectForInfoDictionaryKey("CFBundleShortVersionString") as? String ?: "0.0.0"
+actual fun appVersionName(): String {
+    val bundle = NSBundle.mainBundle
+    return bundle.objectForInfoDictionaryKey("AppVersionName") as? String
+        ?: bundle.objectForInfoDictionaryKey("CFBundleShortVersionString") as? String
+        ?: "0.0.0"
+}
 
 actual val analyticsEnabled: Boolean = true
 
