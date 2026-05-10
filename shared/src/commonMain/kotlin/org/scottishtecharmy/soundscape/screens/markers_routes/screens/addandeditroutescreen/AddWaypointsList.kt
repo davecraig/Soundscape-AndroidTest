aeace553b4725b275a4c546345f79bdaf648de8d
Luke Duncan <luke.william.duncan@gmail.com>
2026-05-10 20:30:30 +0100

    - Created ViewModel to hold list state

--- shared/src/commonMain/kotlin/org/scottishtecharmy/soundscape/screens/markers_routes/screens/addandeditroutescreen/AddWaypointsList.kt
+++ shared/src/commonMain/kotlin/org/scottishtecharmy/soundscape/screens/markers_routes/screens/addandeditroutescreen/AddWaypointsList.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
@@ -29,6 +30,8 @@ import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.liveRegion
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.jetbrains.compose.resources.stringResource
 import org.scottishtecharmy.soundscape.platform.ioDispatcher
 import kotlinx.coroutines.launch
@@ -48,6 +51,11 @@ import org.scottishtecharmy.soundscape.screens.talkbackHint
 import org.scottishtecharmy.soundscape.utils.process
 import org.scottishtecharmy.soundscape.resources.*
 
+class AddWaypointsListViewModel : ViewModel() {
+    private val _listState = LazyListState()
+    val listState = _listState
+}
+
 @Composable
 fun AddWaypointsList(
     uiState: AddAndEditRouteUiState,
@@ -56,7 +64,8 @@ fun AddWaypointsList(
     userLocation: LngLatAlt?,
     onSelectLocation: (LocationDescription) -> Unit,
     onToggleMember: (LocationDescription) -> Unit,
-    getCurrentLocationDescription: () -> LocationDescription
+    getCurrentLocationDescription: () -> LocationDescription,
+    addWaypointsListViewModel: AddWaypointsListViewModel = viewModel()
 ) {
     // Create our list of locations, with those already in the route first
     val locations = remember(uiState.routeMembers, uiState.markers) {
@@ -99,6 +108,7 @@ fun AddWaypointsList(
     var fetchingLocation by remember { mutableStateOf(false) }
 
     LazyColumn(
+        state = addWaypointsListViewModel.listState,
         modifier = Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(spacing.tiny),
     ) {
