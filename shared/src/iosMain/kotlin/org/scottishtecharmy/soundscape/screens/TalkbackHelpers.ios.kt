3ab402d9d7bea9f0933c24541adaf3505089cc14
Dave Craig <davecraig@unbalancedaudio.com>
2026-05-11 13:58:30 +0100

    Default to "Double tap to activate" on TalkBack if there's no hint

--- shared/src/iosMain/kotlin/org/scottishtecharmy/soundscape/screens/TalkbackHelpers.ios.kt
+++ shared/src/iosMain/kotlin/org/scottishtecharmy/soundscape/screens/TalkbackHelpers.ios.kt
@@ -6,12 +6,15 @@ import androidx.compose.ui.semantics.onClick
 import androidx.compose.ui.semantics.semantics
 import org.jetbrains.compose.resources.stringResource
 import org.scottishtecharmy.soundscape.resources.Res
+import org.scottishtecharmy.soundscape.resources.talkback_default_activate
 import org.scottishtecharmy.soundscape.resources.talkback_double_tap_prefix
 
 @Composable
 actual fun Modifier.talkbackHint(hint: String): Modifier {
     val prefix = stringResource(Res.string.talkback_double_tap_prefix)
+    val fallback = stringResource(Res.string.talkback_default_activate)
+    val effective = hint.ifEmpty { fallback }
     return semantics {
-        onClick(label = prefix + hint, action = { false })
+        onClick(label = prefix + effective, action = { false })
     }
 }
