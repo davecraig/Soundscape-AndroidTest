90b6d1aa431e5bfbdcef654ca1a9cbcc0178bb71
davecraig <8530624+davecraig@users.noreply.github.com>
2026-06-04 13:58:57 +0000

    Bump version to 1.0.13, version code 201

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,8 @@ android {
         applicationId = "org.scottishtecharmy.soundscape"
         minSdk = 30
         targetSdk = 35
-        versionCode = 200
-        versionName = "1.0.12"
+        versionCode = 201
+        versionName = "1.0.13"
 
         // Maintaining this list means that we can exclude translations that aren't complete yet
         resourceConfigurations.addAll(listOf(
